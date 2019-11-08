39e883a40cb568457342b579bf45df6cfd962680
Diego Robles <diego@hemployeed.com>
2019-11-08 17:08:03 -0300

    ref(accounts):add rootadmin account on server start

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,7 +1,14 @@
 import { Meteor } from "meteor/meteor";
+import { Accounts } from "meteor/accounts-base";
 
 import "./api";
 
 Meteor.startup(() => {
   console.log("server started");
+  if (Meteor.users.find().fetch().length === 0) {
+    Accounts.createUser({
+      email: "rootadmin@eleven.bot",
+      password: "rootadmin"
+    });
+  }
 });
