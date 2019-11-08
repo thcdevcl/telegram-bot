606f53e0ddbf048e643952961b3be395b76e617a
Diego Robles <diego@hemployeed.com>
2019-11-08 02:40:30 -0300

    feat(router):add currentUser to global context

--- imports/ui/components/router/index.js
+++ imports/ui/components/router/index.js
@@ -1,20 +1,45 @@
 import React from "react";
+import gql from "graphql-tag";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { graphql } from "react-apollo";
+
+import { GlobalContextProvider } from "../../../startup/client/App";
 
 import Index from "../../pages/IndexPage";
 
-function Router() {
+import Spinner from "../utils/Spinner";
+import ScrollTop from "../utils/ScrollTop";
+
+function Router({ loading, currentUser }) {
+  if (loading) return <Spinner />;
   return (
     <BrowserRouter>
-      <Switch>
-        <Route
-          exact
-          path={Meteor.settings.public.router.index.PATH}
-          component={Index}
-        />
-      </Switch>
+      <GlobalContextProvider value={{ currentUser }}>
+        <ScrollTop>
+          <Switch>
+            <Route
+              exact
+              path={Meteor.settings.public.router.index.PATH}
+              component={Index}
+            />
+          </Switch>
+        </ScrollTop>
+      </GlobalContextProvider>
     </BrowserRouter>
   );
 }
 
-export default Router;
+const CURRENT_USER = gql`
+  query currentUser {
+    currentUser {
+      _id
+    }
+  }
+`;
+
+export default graphql(CURRENT_USER, {
+  options: {
+    fetchPolicy: "cache-and-network"
+  },
+  props: ({ data }) => ({ ...data })
+})(Router);
