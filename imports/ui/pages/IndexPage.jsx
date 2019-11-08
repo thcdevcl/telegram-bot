312c849ea6f8643dfb43fdade7dfbc6ee6651733
Diego Robles <diego@hemployeed.com>
2019-11-08 15:45:39 -0300

    feat(index):add signin button

--- imports/ui/pages/IndexPage.jsx
+++ imports/ui/pages/IndexPage.jsx
@@ -1,3 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default () => <h1>{Meteor.settings.public.app.NAME_SHORT}</h1>;
+import { Grid, Button } from "@material-ui/core";
+
+export default () => (
+  <Grid container justify="center" style={{ padding: 16 }}>
+    <Button
+      variant="contained"
+      size="large"
+      color="secondary"
+      component={Link}
+      to={Meteor.settings.public.router.sign_in.PATH}
+    >
+      {Meteor.settings.public.router.sign_in.TITLE}
+    </Button>
+  </Grid>
+);
