f70db4e6bc5113bc65b07c317b2b168e6aa8e04b
Diego Robles <diego@hemployeed.com>
2019-12-05 15:12:46 -0300

    ref(home):redirect to groups if telethonapi success;

--- imports/ui/pages/HomePage.jsx
+++ imports/ui/pages/HomePage.jsx
@@ -1,16 +1,23 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 import { Grid } from "@material-ui/core";
 
+import { GlobalContextConsumer } from "../../startup/client/App";
+
 import Page from "../layouts/PageLayout";
-import Groups from "../components/cards/GroupsCard";
 import TelethonAPI from "../components/cards/TelethonAPICard";
 
 export default () => (
   <Page headline={Meteor.settings.public.router.index.home.TITLE}>
     <Grid container spacing={2} style={{ width: "100%", paddingLeft: 16 }}>
-      <TelethonAPI />
-      <Groups />
+      <GlobalContextConsumer>
+        {({ telethonapi }) => {
+          if (telethonapi.connected && telethonapi.authorized)
+            return <Redirect to={Meteor.settings.public.router.groups.PATH} />;
+          return <TelethonAPI />;
+        }}
+      </GlobalContextConsumer>
     </Grid>
   </Page>
 );
