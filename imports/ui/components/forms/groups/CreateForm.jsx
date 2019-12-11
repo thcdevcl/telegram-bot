280102516d6f1d64ed90a45f80f474ebabd9064c
Diego Robles <diego@hemployeed.com>
2019-12-11 17:32:01 -0300

    wip(group):add formik to create

--- imports/ui/components/forms/groups/CreateForm.jsx
+++ imports/ui/components/forms/groups/CreateForm.jsx
@@ -1,13 +1,51 @@
 import React from "react";
+import { Formik } from "formik";
 
-import { Button, Grid } from "@material-ui/core";
+import { Button, Grid, TextField } from "@material-ui/core";
+
+import Spinner from "../../utils/Spinner";
 
 export default ({ onCancel }) => (
-  <Grid container>
-    <form>
-      <Button color="default" variant="contained" onClick={() => onCancel()}>
-        Cancel
-      </Button>
-    </form>
-  </Grid>
+  <Formik
+    initialValues={{ name: "" }}
+    onSubmit={({ name }, { setSubmitting }) => {
+      setSubmitting(true);
+    }}
+  >
+    {({ values, handleChange, handleSubmit, isSubmitting }) => {
+      if (isSubmitting) return <Spinner />;
+      const { name } = values;
+      return (
+        <form onSubmit={handleSubmit}>
+          <Grid container>
+            <TextField
+              id="name"
+              margin="dense"
+              variant="outlined"
+              values={name}
+              onChange={handleChange}
+              fullWidth
+              placeholder="Name"
+              required
+            />
+            <Grid item xs={12}>
+              <Grid container justify="flex-end">
+                <Button
+                  color="default"
+                  variant="contained"
+                  onClick={() => onCancel()}
+                  style={{ marginRight: 8 }}
+                >
+                  {Meteor.settings.public.forms.commons.CANCEL_BTN_LBL}
+                </Button>
+                <Button color="secondary" variant="contained" type="submit">
+                  {Meteor.settings.public.forms.commons.SUBMIT_BTN_LBL}
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </form>
+      );
+    }}
+  </Formik>
 );
