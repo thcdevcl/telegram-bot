226de6a85fbb5a1ce703d1c0c4ab74353caaaba9
Diego Robles <diego@hemployeed.com>
2019-12-12 18:08:24 -0300

    feat(group):add create group

--- imports/api/groups/resolvers.js
+++ imports/api/groups/resolvers.js
@@ -1,10 +1,26 @@
+import Groups from "./Groups";
+
 export default {
   Group: {
-    participants: async ({ id }, args, { dataSources }) =>
-      await dataSources.TelethonAPI.getParticipants(id)
+    id: ({ _id, id }, args, ctx) => (id ? id : _id),
+    participants: async (
+      { _id, id, participantids },
+      args,
+      { dataSources }
+    ) => {
+      return _id
+        ? participantids.map(
+            async id => await dataSources.TelethonAPI.getEntity(id)
+          )
+        : await dataSources.TelethonAPI.getParticipants(id);
+    }
   },
   Query: {
     group: async (obj, { id }, { dataSources }) =>
       await dataSources.TelethonAPI.getEntity(id)
+  },
+  Mutation: {
+    createGroup: (obj, { group }, { user }) =>
+      Groups.insert({ name: group.name, owner: user._id, participantids: [] })
   }
 };
--- imports/ui/components/forms/groups/CreateForm.jsx
+++ imports/ui/components/forms/groups/CreateForm.jsx
@@ -1,51 +1,82 @@
 import React from "react";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
 import { Formik } from "formik";
 
 import { Button, Grid, TextField } from "@material-ui/core";
 
+import Notify from "../../../../modules/notification";
+
 import Spinner from "../../utils/Spinner";
 
+const CREATE_GROUP = gql`
+  mutation createGroup($group: GroupInput!) {
+    createGroup(group: $group) {
+      id
+    }
+  }
+`;
+
 export default ({ onCancel }) => (
-  <Formik
-    initialValues={{ name: "" }}
-    onSubmit={({ name }, { setSubmitting }) => {
-      setSubmitting(true);
-    }}
-  >
-    {({ values, handleChange, handleSubmit, isSubmitting }) => {
-      if (isSubmitting) return <Spinner />;
-      const { name } = values;
+  <Mutation mutation={CREATE_GROUP}>
+    {(createGroup, { error, loading }) => {
+      if (loading) return <Spinner />;
+      if (error) return `Error: ${error}`;
       return (
-        <form onSubmit={handleSubmit}>
-          <Grid container>
-            <TextField
-              id="name"
-              margin="dense"
-              variant="outlined"
-              values={name}
-              onChange={handleChange}
-              fullWidth
-              placeholder="Name"
-              required
-            />
-            <Grid item xs={12}>
-              <Grid container justify="flex-end">
-                <Button
-                  color="default"
-                  variant="contained"
-                  onClick={() => onCancel()}
-                  style={{ marginRight: 8 }}
-                >
-                  {Meteor.settings.public.forms.commons.CANCEL_BTN_LBL}
-                </Button>
-                <Button color="secondary" variant="contained" type="submit">
-                  {Meteor.settings.public.forms.commons.SUBMIT_BTN_LBL}
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </form>
+        <Formik
+          initialValues={{ name: "" }}
+          onSubmit={({ name }, { setSubmitting }) => {
+            setSubmitting(true);
+            const group = { name };
+            createGroup({ variables: { group } })
+              .then(() => {
+                Notify();
+                onCancel();
+              })
+              .catch(error => Notify({ error }));
+          }}
+        >
+          {({ values, handleChange, handleSubmit, isSubmitting }) => {
+            if (isSubmitting) return <Spinner />;
+            const { name } = values;
+            return (
+              <form onSubmit={handleSubmit}>
+                <Grid container>
+                  <TextField
+                    id="name"
+                    margin="dense"
+                    variant="outlined"
+                    values={name}
+                    onChange={handleChange}
+                    fullWidth
+                    placeholder="Name"
+                    required
+                  />
+                  <Grid item xs={12}>
+                    <Grid container justify="flex-end">
+                      <Button
+                        color="default"
+                        variant="contained"
+                        onClick={() => onCancel()}
+                        style={{ marginRight: 8 }}
+                      >
+                        {Meteor.settings.public.forms.commons.CANCEL_BTN_LBL}
+                      </Button>
+                      <Button
+                        color="secondary"
+                        variant="contained"
+                        type="submit"
+                      >
+                        {Meteor.settings.public.forms.commons.SUBMIT_BTN_LBL}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </form>
+            );
+          }}
+        </Formik>
       );
     }}
-  </Formik>
+  </Mutation>
 );
