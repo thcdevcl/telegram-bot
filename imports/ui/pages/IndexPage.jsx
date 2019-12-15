a1e0d743cad91e175dbe5001f0d7ef9075bec6d2
Diego Robles <diego@hemployeed.com>
2019-12-14 22:50:02 -0300

    feat(accounts):add signup

--- imports/ui/pages/IndexPage.jsx
+++ imports/ui/pages/IndexPage.jsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Grid } from "@material-ui/core";
+import { Button, Grid } from "@material-ui/core";
 
 import Signin from "../components/forms/accounts/SigninForm";
+import Signup from "../components/forms/accounts/SignupForm";
 
-export default () => (
-  <Grid container justify="center" style={{ padding: 16 }}>
-    <Grid item xs={9} sm={6}>
-      <Signin />
+export default () => {
+  const [state, setState] = useState({ signup: false });
+  const { signup } = state;
+  const toggleForm = () => setState({ signup: !signup });
+  return (
+    <Grid container justify="center" style={{ padding: 16 }}>
+      <Grid item xs={9} sm={6}>
+        {signup ? <Signup /> : <Signin />}
+        {!signup && (
+          <Grid container justify="center" style={{ marginTop: 16 }}>
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={() => toggleForm()}
+            >
+              Sign Up
+            </Button>
+          </Grid>
+        )}
+      </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
